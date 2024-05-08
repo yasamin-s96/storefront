22a8864faa9d5c7c8400f7914dfeb29ffcf6dbf5
Yasamin <yasamin.s96@gmail.com>
2024-05-08 18:09:11 +0330

    Database settings for production

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -3,6 +3,17 @@ from .common import *
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["buy.now-noorganizationnn-47235bf4.koyeb.app"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "storefront",
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": "ep-crimson-wind-a42v1138.us-east-1.pg.koyeb.app",
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
