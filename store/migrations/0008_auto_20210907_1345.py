68efb86b06b58fb50769ef90a745a8db0511aa00
Yasamin <yasamin.s96@gmail.com>
2024-05-09 12:07:55 +0330

    modified migrations 0007 and 0008

--- store/migrations/0008_auto_20210907_1345.py
+++ store/migrations/0008_auto_20210907_1345.py
@@ -7,17 +7,17 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0007_alter_cart_id'),
+        ("store", "0007_alter_cart_id"),
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='cartitem',
-            name='cart',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='store.cart'),
-        ),
-        migrations.AlterUniqueTogether(
-            name='cartitem',
-            unique_together={('cart', 'product')},
-        ),
+        # migrations.AlterField(
+        #     model_name='cartitem',
+        #     name='cart',
+        #     field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='store.cart'),
+        # ),
+        # migrations.AlterUniqueTogether(
+        #     name='cartitem',
+        #     unique_together={('cart', 'product')},
+        # ),
     ]
